8afb860258ece629a7c3992ca3a18b3ec8c9ccf4
Carlos <ctaboadafe@ikzubirimanteo.com>
2020-03-03 17:31:42 +0100

    Pruebas de primer repositorio en git desde eclipse

---
+++ Git/src/git/HolaMundo.java
@@ -0,0 +1,14 @@
+package git;
+/*
+  * Ejemplo HolaMundo 
+  * Imprime el mensaje "Hola, Mundo!" 
+*/
+    
+public class HolaMundo {
+    public static void main( String[] args ){
+       
+        System.out.println("Hola, Mundo desde Java!");
+        
+    }
+
+}
